8812f30d1ea9be6dbb7d7c1a29482a5f3f346691
Giedrius Budnikas <giedrius.budnikas@gmail.com>
2022-07-02 15:24:20 +0300

    Channel model + factory

---
+++ database/factories/ChannelFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class ChannelFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'youtube_id' => Str::random(24),
+            'name' => fake()->sentence(),
+            'views_first_hour_median' => rand(100, 10000),
+        ];
+    }
+}
