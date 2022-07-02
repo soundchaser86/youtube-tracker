47a0fc51126f99714f0952e72fd38dc5d1a37e60
Giedrius Budnikas <giedrius.budnikas@gmail.com>
2022-07-02 17:22:03 +0300

    ChannelSeeder

---
+++ database/seeders/ChannelSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Channel;
+use App\Models\Video;
+use App\Models\VideoStatistic;
+use App\Models\VideoTag;
+use Illuminate\Database\Seeder;
+
+class ChannelSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Channel::factory()
+            ->count(5)
+            ->has(
+                Video::factory()
+                    ->count(10)
+                    ->has(VideoStatistic::factory(), 'statistic')
+                    ->has(VideoTag::factory()->count(5), 'tags')
+            )
+            ->create();
+    }
+}
