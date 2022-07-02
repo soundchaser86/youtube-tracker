802f17219d538cf9a75a7b881539785c5dd491e6
Giedrius Budnikas <giedrius.budnikas@gmail.com>
2022-07-02 15:41:38 +0300

    VideoTag model + factory

---
+++ database/factories/VideoTagFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Video;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\VideoTag>
+ */
+class VideoTagFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'video_id' => Video::factory(),
+            'name' => fake()->word(),
+        ];
+    }
+}
