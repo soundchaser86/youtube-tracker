a1e6eb7364c100ead71733bccf3f4ca251e2c267
Giedrius Budnikas <giedrius.budnikas@gmail.com>
2022-07-06 15:04:37 +0300

    implemented pagination

--- resources/js/components/VideoListComponent.vue
+++ resources/js/components/VideoListComponent.vue
@@ -8,28 +8,40 @@
         </tr>
         </thead>
         <tbody>
-        <tr v-for="video in videos">
+        <tr v-for="video in data.data">
             <td>{{ video.name }}</td>
             <td>{{ video.channel_name }}</td>
             <td>{{ video.views_first_hour }}</td>
         </tr>
         </tbody>
     </table>
+
+    <Pagination :data="data" @pagination-change-page="getAll" />
 </template>
 
 <script>
+import LaravelVuePagination from 'laravel-vue-pagination';
+
 export default {
+    components: {
+        'Pagination': LaravelVuePagination
+    },
     data() {
         return {
-            videos: []
+            data: {},
         }
     },
-    created() {
-        this.axios
-            .get('http://youtube-tracker.test/videos/getAll')
-            .then(response => {
-                this.videos = response.data.data;
-            });
+    methods: {
+        getAll(page = 1) {
+            this.axios
+                .get('http://youtube-tracker.test/videos/getAll?page=' + page)
+                .then(response => {
+                    this.data = response.data;
+                });
+        }
+    },
+    mounted() {
+        this.getAll();
     },
 }
 </script>
